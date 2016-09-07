b7c3a7c926536bf73b21cf1e3ac07148129c1fa7
Natalia Vladimirova <Natalia_Vladimirova@epam.com>
2016-09-07 11:26:00 +0300

    Initial implementation.

---
+++ Task3/Task3/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task3.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View("Person");
+        }
+
+        [HttpPost]
+        public ActionResult GetFooter(string action)
+        {
+            if (action == "yes")
+            {
+                //ExcelReport(model);
+            }
+            else if (action == "no")
+            {
+
+              //  return GetReport(model);
+            }
+
+          //  return PartialView("_Footer");
+        }
+
+    }
+}
---
+++ Task3/Task3/Repository/CustomRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Task3.Repository
+{
+    public class CustomRepository
+    {
+        public static List<string> Sides { get; set; } = new List<string>()
+        {
+            "White",
+            "Black"
+        };
+
+
+
+    }
+
+    public enum Sides
+    {
+        White,
+        Black
+    }
+}
